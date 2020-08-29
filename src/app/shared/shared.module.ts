fa956e46de4c96f00256083b6f8931c3edb90d89
Hatsanai <hatsanai.d.tong@gmail.com>
2020-08-29 11:09:09 +0700

    feat: use skeleton loading instead of spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,24 +3,25 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
 import { CountryCardListComponent } from './components/country-card-list/country-card-list.component';
 import { CountryCardComponent } from './components/country-card/country-card.component';
-import { SpinnerComponent } from './components/spinner/spinner.component';
 import { ErrorComponent } from './components/error/error.component';
+import { CountryCardSkeletonComponent } from './components/country-card-skeleton/country-card-skeleton.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @NgModule({
   declarations: [
     SearchComponent,
     CountryCardListComponent,
     CountryCardComponent,
-    SpinnerComponent,
     ErrorComponent,
+    CountryCardSkeletonComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, NgxSkeletonLoaderModule],
   exports: [
     SearchComponent,
     CountryCardListComponent,
     CountryCardComponent,
-    SpinnerComponent,
     ErrorComponent,
+    CountryCardSkeletonComponent,
   ],
 })
 export class SharedModule {}
